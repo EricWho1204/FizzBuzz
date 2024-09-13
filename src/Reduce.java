0330c17ff68cd884e44367f24b296043b1f56bcf
Eric Hu <erichu@MacBook-Air-240.local>
2024-09-13 17:26:05 -0400

    refactored to extract helper method

---
+++ src/Reduce.java
@@ -0,0 +1,21 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int count = 0;
+        int n = 100;
+        while (n!=0){
+            if (n%2==0){
+                n=n/2;
+                count++;
+
+            }
+            else{
+                count++;
+                n=n-1;
+            }
+
+        }
+        System.out.println(count);
+
+
+    }
+}
